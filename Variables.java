fcca1bf3853d3bf52c45cc9605a5c3a7631fffbe
poornika26-commit <poornikar6@gmail.com>
2026-07-31 20:39:58 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,23 @@
+
+
+public class Variables {
+    public static void main(String[] args) {
+        int num = 10;
+        double decimal =3.14;
+        long number = 122333333;
+        String name ="ROCK PAPER";
+        char ch ='Q';
+        boolean b = true;
+
+        System.out.println("Integer:" + num);
+        
+        System.out.println("Decimal:" + decimal);
+        System.out.println("Long:" + number);
+        System.out.println("Name:"+ name);
+        System.out.println("Character:"+ ch);
+        System.out.println("Boolean:" +b);
+
+
+    }
+    
+}
